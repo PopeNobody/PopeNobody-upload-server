7158c94bc917d8258dbb03e1ab0037b5fda4ae6f
nobody <github2@invisiblehand.church>
2022-09-06 05:38:14 +0000

    all output is not on stderr, except output for caller

--- checkret.cc
+++ checkret.cc
@@ -1,4 +1,5 @@
 #include <arpa/inet.h>
+#include <sys/mman.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <netinet/in.h>
@@ -195,15 +196,15 @@ int checkret::bind_and_accept(const char *addr, int port) {
   res=xlisten(sock,1);
   while(true){
     socklen_t socklen;
-    dprintf(1,"%s: accepting on %s:%d\n",now(),addr,port);
+    dprintf(2,"%s: accepting on %s:%d\n",now(),addr,port);
     int sockfd=xaccept(sock,(sockaddr*)&sin_addr,&socklen);
-    dprintf(1,"%s: connection from: %s %d\n",
+    dprintf(2,"%s: connection from: %s %d\n",
         now(),
         inet_ntoa(sin_addr.sin_addr),ntohs(sin_addr.sin_port));
     xsetsockopt(sockfd, IPPROTO_TCP, TCP_NODELAY, (char *) &flag, sizeof(int));
     if(forking()){
       int pid=xfork();
-      dprintf(1,"%d => %d\n",getpid(),pid);
+      dprintf(2,"%d => %d\n",getpid(),pid);
       if(!pid) {
         xclose(sock);
         return sockfd;
@@ -225,19 +226,6 @@ void checkret::xpipe(int fds[2]) {
   if(pipe(fds))
     pexit(13,"pipe");
 };
-//   int checkret::xexecve(const char *pathname, char *const argv[], char *const envp[])
-//   {
-//     execve(pathname,argv,envp);
-//     pexit(14,"execve");
-//   }
-//   int checkret::xexecveat(int dirfd, const char *pathname,
-//       char *const argv[], char *const envp[],
-//       int flags)
-//   {
-//     execve(pathname, argv, envp);
-//     pexit(14,"execveat");
-//     exit(-1);
-//   };
 void checkret::xmkdirat(int dirfd, const char *pathname, mode_t mode){
   if(mkdirat(dirfd,pathname,mode))
     pexit(14,"mkdir");
@@ -253,4 +241,24 @@ const char *checkret::now()
       gm.tm_year+1900,gm.tm_mon+1,gm.tm_mday,
       gm.tm_hour,gm.tm_min,gm.tm_sec);
   return res.beg();
+};
+void *checkret::xmmap(void *addr, size_t length, int prot, int flags,
+    int fd, off_t offset)
+{
+  void *res=mmap(addr,length,prot,flags,fd,offset);
+  if(res==MAP_FAILED)
+    pexit(1,"mmap");
+  return res;
+};
+void checkret::xmunmap(void *addr, size_t length){
+  if(munmap(addr,length))
+    pexit(1,"munmap");
+};
+itr_pair_t checkret::xmmap_file(const char*fname) {
+  itr_pair_t res;
+  int fd=xopenat(AT_FDCWD,fname,O_RDONLY);
+  size_t size=lseek(fd,0,SEEK_END);
+  res.beg=(const char*)xmmap(0,size,PROT_READ,MAP_PRIVATE,fd,0);
+  res.end=res.beg+size;
+  return res;
 };
